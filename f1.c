a0039fc3743229fce8dbb1714c0d384d8210964f
athenelon <velimirovicsinisa1@gmail.com>
2021-07-04 13:35:32 +0200

    second commit on feature2Dev branch. Changed f1.c and f2.c

--- f1.c
+++ f1.c
@@ -1,2 +1,3 @@
 //This is feature1 file.
 //first feature change on master branch
+//first feature change on masterfeature2Dev branch
--- f2.c
+++ f2.c
@@ -1,1 +1,2 @@
 //Feature 2 dev start
+//Feature 2 second commit
